663e56b6d979f8b42673c3c64b41996f820f38e1
cmsusergit <45680530+cmsusergit@users.noreply.github.com>
2023-09-16 11:07:16 +0530

    2 files modified

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const uploadFile = async (fileEvent) => {
+  const fileList = fileEvent.target.files;
+
+  let returnValue = '';
+  try {
+    if (!fileList || fileList.length === 0) {
+      throw new Error('You must select an image to upload.');
+    }
+    const file = fileList[0];
+    const fileExt = file.name.split('.').pop();
+    const filePath = `${Math.random()}.${fileExt}`;
+
+    console.log('----', filePath);
+    console.log('----', file);
+    let { error } = await _supabase.storage
+      .from('form-photo')
+      .upload(`formdt/${filePath}`, file);
+    if (error) {
+      throw error;
+    }
+    returnValue = filePath;
+  } catch (error) {
+    if (error instanceof Error) {
+      alert(error.message);
+      returnValue = '----';
+    }
+  } finally {
+  }
+  return returnValue;
+};
 const getInfo = async () => {
   //
   // const { data, error } = await _supabase.storage.from('form-photo').list('.');
